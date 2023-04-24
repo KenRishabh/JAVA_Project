bdbb8e07ff53669252891f56ecaf9fe45366821c
KenRishabh <rishabhbvishwakarma101@gmail.com>
2023-04-24 22:40:05 +0530

    For Looping

--- src/Loop.java
+++ src/Loop.java
@@ -4,12 +4,17 @@ public class Loop {
 //        if(a==10){
 //            System.out.println("I Love India");
 //        }
-        int count = 1;
-        while (count != 5) {
-            System.out.println(count);
-            count++;
-        }
 
+        //while loop
+//        int count = 1;
+//        while (count != 5) {
+//            System.out.println(count);
+//            count++;
+//        }
+        // for loop
+        for(int count= 1; count !=5; count++){
+            System.out.println("ILoveIndia");
+        }
 
     }
 }
