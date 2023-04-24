a9315ec9d63113b1c2bf780b30dc1a183abbdf8b
KenRishabh <rishabhbvishwakarma101@gmail.com>
2023-04-24 22:38:08 +0530

    If & while looping

---
+++ src/Loop.java
@@ -0,0 +1,15 @@
+public class Loop {
+    public static void main(String[] args) {
+//        int a =10;
+//        if(a==10){
+//            System.out.println("I Love India");
+//        }
+        int count = 1;
+        while (count != 5) {
+            System.out.println(count);
+            count++;
+        }
+
+
+    }
+}
