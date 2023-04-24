e52f6c2d8cb021265c4a95006d4eb88074b66ef7
KenRishabh <rishabhbvishwakarma101@gmail.com>
2023-04-24 22:20:04 +0530

    Typecasting
    byte to int

--- src/TypeCasting.java
+++ src/TypeCasting.java
@@ -5,8 +5,22 @@ import java.util.Scanner;
 public class TypeCasting {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        float num = input.nextFloat();
+//        float num = input.nextFloat();
+//        System.out.println(num);
+//
+        int num = (int)(67.99f);  // typecasting
         System.out.println(num);
-    }
 
+//        automatic type promotion in expressions
+//        int a = 257;
+//        byte b = (byte) (a);
+//        System.out.println(b);
+//
+        byte a = 40;
+        byte b = 50;
+        byte c = 100;
+          int d = (a*b)/c;
+        System.out.println(d);
 }
+}
+// It won't convert high value to lower value int-float(Possible) but float to int(Not Possible)
