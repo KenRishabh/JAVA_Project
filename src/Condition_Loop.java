2de7c93727189dd30c634b68e81a3724e4d2d098
KenRishabh <rishabhbvishwakarma101@gmail.com>
2023-04-25 19:51:23 +0530

    If & while looping

---
+++ src/Condition_Loop.java
@@ -0,0 +1,21 @@
+public class Condition_Loop {
+    public static void main(String[] args) {
+        int salary = 50000;
+       /* if (salary > 10000) {
+            salary = salary + 2000;
+        } else {
+            salary = salary + 1000;
+        }*/
+
+        if(salary>100000){
+            salary=salary+1000;
+        }
+        else if(salary<100000){
+            salary=salary+5000;
+        }
+        else{
+            salary=salary+100;
+        }
+        System.out.println(salary);
+    }
+}
